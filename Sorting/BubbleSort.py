594695220a466626c6cdb410812468395a4389a9
ROHIT SAHU <rohitsahu98377@gmail.com>
2026-05-09 17:57:02 +0530

    Added Merge Sort Algorithm

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -6,11 +6,14 @@ def bubbleSort(arr):
     n = len(arr)
 
     for i in range(n):
-
+        flag=False;
         for j in range(0, n-i-1):
 
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                flag=True;
+        if flag==False:
+            break;  
 
 bubbleSort(arr)
 print("Sorted array is:", arr)
