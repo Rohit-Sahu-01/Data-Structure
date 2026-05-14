687dd42c78c323a40ea033409a16d999132d941e
ROHIT SAHU <rohitsahu98377@gmail.com>
2026-05-14 23:57:48 +0530

    Create RadixSort.py

---
+++ Sorting/RadixSort.py
@@ -0,0 +1,33 @@
+arr=[170, 45, 75, 90, 2, 802, 100]
+
+def countingSort(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+
+    for i in range(n):
+        index = (arr[i] // exp) % 10
+        count[index] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = n - 1
+    while i >= 0:
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+        i -= 1
+
+    for i in range(n):
+        arr[i] = output[i]
+
+def RadixSort(arr):
+    max1 = max(arr)
+    exp = 1
+    while max1/exp > 1:
+        countingSort(arr, exp)
+        exp *= 10
+
+RadixSort(arr)
+print("Sorted array is:", arr)
